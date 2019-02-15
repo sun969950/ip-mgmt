a79161b695349af0ad39dba0b52f7aaba8c619c4
sun969950 <sun969950@163.com>
2019-02-15 10:05:08 +0800

    w222

--- conf/default.py
+++ conf/default.py
@@ -22,9 +22,9 @@ from django.conf.global_settings import *
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -》点击应用ID -》基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'ip-mgmt'
-APP_TOKEN = '200715ea-e963-4c00-916b-41c2f6d36c1a' # pass ƽ̨token
+APP_TOKEN = 'eed6c8e3-485b-4255-98d2-f9702d023e45' # pass ƽ̨token
 # 蓝鲸智云开发者中心的域名，形如：http://openpaas.bk.com
-BK_PAAS_HOST = 'https://paas.gzbkee.com'
+BK_PAAS_HOST = 'http://paas.gzbk.com'
 
 # 是否启用celery任务
 IS_USE_CELERY = True
--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID + '-test',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'IXjE*rL(sB',                        # 你的数据库password
-        'HOST': '192.168.102.201',                   		   # 数据库HOST
+        'PASSWORD': 'YtX#_]O[6n',                        # 你的数据库password
+        'HOST': '192.168.102.232',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
